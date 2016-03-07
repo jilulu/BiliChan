ef4d2607a17169fccb944cbd240ec004e102771c
James Ji <1stlulu>
2016-03-08 03:19:32 +0800

    infinite scroll!

--- app/src/main/java/xyz/jilulu/bilifun/adapters/KonaAdapter.java
+++ app/src/main/java/xyz/jilulu/bilifun/adapters/KonaAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Callback;
@@ -21,15 +22,15 @@ import xyz.jilulu.bilifun.helpers.KonaObject;
 /**
  * Created by jamesji on 29/2/2016.
  */
-public class KonaAdapter extends RecyclerView.Adapter<KonaAdapter.ViewHolder> {
+public class KonaAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private ArrayList<KonaObject> mDataset;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class CardHolder extends RecyclerView.ViewHolder {
         public CardView mCardView;
         public ImageView img, icon;
         public TextView author, size;
 
-        public ViewHolder(CardView cardView) {
+        public CardHolder(CardView cardView) {
             super(cardView);
             mCardView = cardView;
             img = (ImageView) mCardView.findViewById(R.id.gallery_card_image_view);
@@ -39,54 +40,76 @@ public class KonaAdapter extends RecyclerView.Adapter<KonaAdapter.ViewHolder> {
         }
     }
 
+    public static class ProgressHolder extends RecyclerView.ViewHolder {
+        ProgressBar mProgressBar;
+
+        public ProgressHolder(View v) {
+            super(v);
+            this.mProgressBar = (ProgressBar)v.findViewById(R.id.progressBar);
+        }
+    }
+
     public KonaAdapter(ArrayList<KonaObject> dataset) {
         mDataset = dataset;
     }
 
     @Override
-    public KonaAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                     int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        if (viewType == 1) {
+            return new ProgressHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.progress_item, parent, false));
+        }
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.activity_gallery_card_view, parent, false);
         // set the view's size, margins, paddings and layout parameters
         // ...
-        ViewHolder vh = new ViewHolder((CardView) v);
+        CardHolder vh = new CardHolder((CardView) v);
         return vh;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public int getItemViewType(int position) {
+        return mDataset.get(position)!=null? 0: 1;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        final ViewHolder mViewHolder = holder;
-        Callback imageDownloaded = new Callback() {
-            @Override
-            public void onSuccess() {
-                mViewHolder.icon.setImageResource(R.drawable.view_done);
-            }
 
-            @Override
-            public void onError() {
+        if (holder instanceof ProgressHolder) {
+            ((ProgressHolder) holder).mProgressBar.setIndeterminate(true);
+        } else {
+            final CardHolder mViewHolder = (CardHolder) holder;
+            Callback imageDownloaded = new Callback() {
+                @Override
+                public void onSuccess() {
+                    mViewHolder.icon.setImageResource(R.drawable.view_done);
+                }
 
-            }
-        };
+                @Override
+                public void onError() {
 
-        final KonaObject obj = mDataset.get(position);
-        Picasso.with(holder.mCardView.getContext())
-                .load(obj.getPreviewURL())
-                .into(holder.img, imageDownloaded);
+                }
+            };
 
-        holder.mCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent revolutionaryIntent = new Intent(v.getContext(), RevolutionaryPhotoView.class);
-                revolutionaryIntent.putExtra(Intent.EXTRA_TEXT, obj.getLargeSizeURL());
-                v.getContext().startActivity(revolutionaryIntent);
-            }
-        });
-        holder.author.setText(obj.getAuthor());
-        holder.size.setText(Integer.toString(obj.getFileSize()));
+            final KonaObject obj = mDataset.get(position);
+            Picasso.with(mViewHolder.mCardView.getContext())
+                    .load(obj.getPreviewURL())
+                    .into(mViewHolder.img, imageDownloaded);
+
+            mViewHolder.mCardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent revolutionaryIntent = new Intent(v.getContext(), RevolutionaryPhotoView.class);
+                    revolutionaryIntent.putExtra(Intent.EXTRA_TEXT, obj.getLargeSizeURL());
+                    v.getContext().startActivity(revolutionaryIntent);
+                }
+            });
+            mViewHolder.author.setText(obj.getAuthor());
+            mViewHolder.size.setText(Integer.toString(obj.getFileSize()));
+        }
     }
 
     @Override
