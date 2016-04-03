b0bbc63f3ae743e5fae21acb4fcc57d67b4e29c5
jilulu <1stlulu@gmail.com>
2016-04-03 12:24:35 +0800

    Optimized image download workflow

--- app/src/main/java/xyz/jilulu/bilichan/adapters/KonaTagAdapter.java
+++ app/src/main/java/xyz/jilulu/bilichan/adapters/KonaTagAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.support.v4.util.LruCache;
 import android.support.v7.widget.CardView;
@@ -19,14 +20,16 @@ import android.widget.Toast;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -46,8 +49,12 @@ public class KonaTagAdapter extends RecyclerView.Adapter<KonaTagAdapter.ViewHold
     private RecyclerView parent;
     private LruCache<String, BitmapDrawable> mMemoryCache;
     private File cacheDir;
+    private HashMap<String, JsonArray> konaChanResponses = new HashMap<>();
+    private ArrayList<String> httpRequestInProgress = new ArrayList<>();
+    private Context mContext;
 
-    public KonaTagAdapter(ArrayList<KonaTag> dataSet) {
+    public KonaTagAdapter(ArrayList<KonaTag> dataSet, Context context) {
+        mContext = context;
         this.dataSet = dataSet;
         int maxMemory = (int) Runtime.getRuntime().maxMemory();
         int cacheSize = maxMemory / 2;
@@ -61,7 +68,7 @@ public class KonaTagAdapter extends RecyclerView.Adapter<KonaTagAdapter.ViewHold
     }
 
     public void addBitmapToMemoryCache(String key, BitmapDrawable bitmapDrawable) {
-        if (getBitmapFromMemoryCache(key) == null)
+        if (getBitmapFromMemoryCache(key) == null && bitmapDrawable != null)
             mMemoryCache.put(key, bitmapDrawable);
     }
 
@@ -83,19 +90,19 @@ public class KonaTagAdapter extends RecyclerView.Adapter<KonaTagAdapter.ViewHold
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         final KonaTag currentTag = dataSet.get(position);
-        String _tagName = currentTag.getTagName();
+        String tagString = currentTag.getTagName();
 
-        holder.tag.setText(_tagName);
+        holder.tag.setText(tagString);
         int count = Integer.parseInt(currentTag.getTagCount());
 
         holder.count.setText(count > 1000 ? count / 1000 + "k" : count + "");
         String largeType, betterVisual;
-        int underscoreLocation = _tagName.indexOf('_');
-        largeType = "" + Character.toUpperCase(_tagName.charAt(0)) + Character.toUpperCase(_tagName.charAt(underscoreLocation + 1));
+        int underscoreLocation = tagString.indexOf('_');
+        largeType = "" + Character.toUpperCase(tagString.charAt(0)) + Character.toUpperCase(tagString.charAt(underscoreLocation + 1));
         holder.circledTV.setText(largeType);
-        betterVisual = underscoreLocation == -1 ? Character.toUpperCase(_tagName.charAt(0)) + _tagName.substring(1) :
-                (Character.toUpperCase(_tagName.charAt(0)) + _tagName.substring(1, underscoreLocation) +
-                        " " + Character.toUpperCase(_tagName.charAt(underscoreLocation + 1)) + _tagName.substring(underscoreLocation + 2));
+        betterVisual = underscoreLocation == -1 ? Character.toUpperCase(tagString.charAt(0)) + tagString.substring(1) :
+                (Character.toUpperCase(tagString.charAt(0)) + tagString.substring(1, underscoreLocation) +
+                        " " + Character.toUpperCase(tagString.charAt(underscoreLocation + 1)) + tagString.substring(underscoreLocation + 2));
         holder.betterVisualization.setText(betterVisual);
 
         final Context ct = holder.cv.getContext();
@@ -111,34 +118,13 @@ public class KonaTagAdapter extends RecyclerView.Adapter<KonaTagAdapter.ViewHold
             }
         });
 
-
-        BitmapDrawable[] bmpDrawable = new BitmapDrawable[4];
-        for (int i = 0; i < bmpDrawable.length; i++) {
+        for (int i = 0; i < 4; i++) {
+            holder.imageViews[i].setTag(tagString + i);
             holder.imageViews[i].setImageResource(R.drawable.fav_holder);
-            holder.imageViews[i].setTag(_tagName + i);
-            bmpDrawable[i] = getBitmapFromMemoryCache(_tagName + i);
-            File tempFile = new File(cacheDir, _tagName + i);
-            if (bmpDrawable[i] == null && tempFile.exists()) {
-                try {
-                    BufferedSource source = Okio.buffer(Okio.source(tempFile));
-                    byte[] bmpBytes = source.readByteArray();
-                    addBitmapToMemoryCache(_tagName + i, new BitmapDrawable(BitmapFactory.decodeByteArray(bmpBytes, 0, bmpBytes.length)));
-                    bmpDrawable[i] = getBitmapFromMemoryCache(_tagName + i);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        if (bmpDrawable[0] != null && bmpDrawable[1] != null && bmpDrawable[2] != null && bmpDrawable[3] != null) {
-            for (int i = 0; i < bmpDrawable.length; i++)
-                holder.imageViews[i].setImageDrawable(bmpDrawable[i]);
-        } else{
-            (new FetchThumbnailTask()).execute(_tagName);
-            Log.d("BindViewHolder", position + ": ViewHolder Binded");
+            FetchThumbnailTask littleTask = new FetchThumbnailTask(tagString + i);
+            littleTask.execute();
         }
     }
-
 
     @Override
     public int getItemCount() {
@@ -167,88 +153,149 @@ public class KonaTagAdapter extends RecyclerView.Adapter<KonaTagAdapter.ViewHold
         }
     }
 
-    class FetchThumbnailTask extends AsyncTask<String, Integer, Void> {
-        private final String[] url_json = {"http://konachan.net/post.json?tags=", "%20order:score%20rating:safe"};
-        private String tag;
-        private ConnectionPool pool = new ConnectionPool(1, 5, TimeUnit.MINUTES);
+    class FetchThumbnailTask extends AsyncTask<Void, Integer, Void> {
 
-        private final OkHttpClient client = new OkHttpClient.Builder()
+        /*
+            There're in total three case senarios to be considered:
+            1. In LRU memory cache. -> Fetch directly.
+            2. In local disk cache. -> Fetch and add to LRU cache to speed up later IO.
+            3. In the cloud.        -> Fetch and add to both LRU and disk cache.
+         */
+
+        final String[] url_json = {"http://konachan.net/post.json?tags=", "%20order:score%20rating:safe"};
+
+        ConnectionPool pool = new ConnectionPool(1, 5, TimeUnit.MINUTES);
+        final OkHttpClient client = new OkHttpClient.Builder()
                 .connectionPool(pool)
                 .build();
 
+        private String tag;
+
+        private String imagePreviewLink;
+
+        public FetchThumbnailTask(String tag) {
+            this.tag = tag;
+        }
+
+        private Bitmap fetchedBitmap;
+
         @Override
-        protected Void doInBackground(final String... params) {
-            tag = params[0];
-            String url = url_json[0] + tag + url_json[1];
-            Log.d("OKHTTP", url);
-            Request req = new Request.Builder()
-                    .url(url)
-                    .build();
-            client.newCall(req).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    publishProgress(-1);
+        protected Void doInBackground(Void... params) {
+            final BitmapDrawable drawable = getBitmapFromMemoryCache(tag);
+            if (drawable != null) { // CASE 1 IN LRU CACHE
+                fetchedBitmap = drawable.getBitmap();
+                Log.d("LRU", tag);
+                return null;
+            } else if (new File(cacheDir, tag).exists()) { // CASE 2 IN DISK CACHE
+                try {
+                    BufferedSource bs = Okio.buffer(Okio.source(new File(cacheDir, tag)));
+                    byte[] bytes = bs.readByteArray();
+                    addBitmapToMemoryCache(tag, new BitmapDrawable(BitmapFactory.decodeByteArray(bytes, 0, bytes.length)));
+                    fetchedBitmap = getBitmapFromMemoryCache(tag).getBitmap();
+                    Log.d("DiskCache", tag);
+                    return null;
+                } catch (IOException ee) { // No disk cache
+                    ee.printStackTrace();
                 }
-
-                @Override
-                public void onResponse(Call call, final Response res) throws IOException {
-                    String json_response = res.body().string();
-                    JsonParser parser = new JsonParser();
-                    JsonArray arr = parser.parse(json_response).getAsJsonArray();
-                    String[] imagePreviewLinks = new String[4];
-                    for (int i = 0; i < imagePreviewLinks.length; i++) {
-                        final int currentDownloadNum = i;
-                        imagePreviewLinks[i] = arr.get(i).getAsJsonObject().get("preview_url").getAsString();
-
-                        final File tempFile = new File(cacheDir, tag + currentDownloadNum);
-
-                        client.newCall(new Request.Builder().url(imagePreviewLinks[i]).build()).enqueue(new Callback() {
-                            @Override
-                            public void onFailure(Call call, IOException e) {
-                                publishProgress(-1);
-                            }
-
-                            @Override
-                            public void onResponse(Call call, Response response) throws IOException {
-                                byte[] responseBytes = response.body().bytes();
-                                Bitmap bmp = BitmapFactory.decodeByteArray(responseBytes, 0, responseBytes.length);
-                                BitmapDrawable drawable = new BitmapDrawable(bmp);
-
-                                BufferedSink sink = Okio.buffer(Okio.sink(tempFile));
-                                sink.write(responseBytes);
-                                sink.close();
-
-                                addBitmapToMemoryCache(tag + currentDownloadNum, drawable);
-                                publishProgress(currentDownloadNum);
-                                Log.d("OKHTTP", "Image No." + (currentDownloadNum + 1) + " downloaded. Added to Cache. ");
-                            }
-                        });
-
+            } else { // CASE 3 IN THE CLOUD
+                final String realTag = tag.substring(0, tag.length() - 1);
+                final int imageNum = Integer.parseInt(tag.substring(tag.length() - 1));
+                if (konaChanResponses.containsKey(realTag)) {
+                    imagePreviewLink = konaChanResponses.get(realTag).get(imageNum).getAsJsonObject().get("preview_url").getAsString();
+                    return null;
+                } else {
+                    boolean DOMDownloadedByAnotherThread = false;
+                    while (httpRequestInProgress.contains(realTag)) {
+                        try {
+                            Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        DOMDownloadedByAnotherThread = true;
                     }
-
+                    if (DOMDownloadedByAnotherThread) {
+                        imagePreviewLink = konaChanResponses.get(realTag).get(imageNum).getAsJsonObject().get("preview_url").getAsString();
+                        return null;
+                    }
+                    Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+                    httpRequestInProgress.add(realTag);
+                    String url = url_json[0] + realTag + url_json[1];
+                    Log.d("OKHTTP", url);
+                    Request req = new Request.Builder().url(url).build();
+                    try {
+                        Response response = client.newCall(req).execute();
+                        String json_response = response.body().string();
+                        JsonParser parser = new JsonParser();
+                        JsonArray arr = parser.parse(json_response).getAsJsonArray();
+                        konaChanResponses.put(realTag, arr);
+                        imagePreviewLink = konaChanResponses.get(realTag).get(imageNum).getAsJsonObject().get("preview_url").getAsString();
+                        httpRequestInProgress.remove(realTag);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
                 }
-
-            });
-
+            }
             return null;
+
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if (imagePreviewLink == null && fetchedBitmap != null) {
+                ImageView imgV = (ImageView) parent.findViewWithTag(tag);
+                if (imgV != null)
+                    imgV.setImageBitmap(fetchedBitmap);
+            }
+            else if (imagePreviewLink != null) {
+                final ImageView imageView = (ImageView) parent.findViewWithTag(tag);
+                if (imageView != null) {
+                    Picasso.with(mContext).load(imagePreviewLink).into(new Target() {
+                        @Override
+                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                            try {
+                                BufferedSink bs = Okio.buffer(Okio.sink(new File(cacheDir, tag)));
+                                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                                bs.write(stream.toByteArray());
+                                bs.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            imageView.setImageBitmap(bitmap);
+                        }
+
+                        @Override
+                        public void onBitmapFailed(Drawable errorDrawable) {
+
+                        }
+
+                        @Override
+                        public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                        }
+                    });
+                }
+            }
         }
 
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
 
-            // Server denied connection
-            if (values[0] == -1) {
-                Toast.makeText(parent.getContext(), "You're hurting KonaChan. ", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            int progressUpdatedItem = values[0];
-            String key = tag + progressUpdatedItem;
-            ImageView imgV = (ImageView) parent.findViewWithTag(key);
-            BitmapDrawable bmpD = getBitmapFromMemoryCache(key);
-            if (imgV != null && bmpD != null) {
-                imgV.setImageDrawable(bmpD);
+            switch (values[0]) {
+                case -1:
+                    Toast.makeText(parent.getContext(), "You're hurting KonaChan. ", Toast.LENGTH_SHORT).show();
+                    break;
+                case 0:
+                    ImageView v = (ImageView) parent.findViewWithTag(tag);
+                    if (v != null)
+                        v.setImageBitmap(
+                                getBitmapFromMemoryCache(tag).getBitmap()
+                        );
+                    break;
             }
         }
 
